feeafa5f3e0defc8b38b43d38b02a9a6894c226e
Riyanat Shittu <riyanat@Riyanats-MacBook-Pro.local>
2021-03-24 22:14:10 +0000

    initial commit

---
+++ src/main/java/io/riyanat/reportingservice/ReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.riyanat.reportingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingServiceApplication.class, args);
+	}
+
+}
